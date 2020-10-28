aa963b49a2cd966537c892002471bca662d9b0ae
Jamie Magee <jamie.magee@gmail.com>
2020-10-28 13:57:47 +0100

    test: set coverage threshold to 0

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
     : ['html', 'text-summary'],
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
     },
   },
   setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
