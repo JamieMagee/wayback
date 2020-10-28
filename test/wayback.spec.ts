7d3af54d714691fecf5369657b4d2bfd592e3c27
Jamie Magee <jamie.magee@gmail.com>
2020-10-28 13:39:45 +0100

    test: simple test for wayback.ts

--- test/wayback.spec.ts
+++ test/wayback.spec.ts
@@ -1,9 +1,21 @@
+import nock from 'nock';
+import WayBack from '../src/wayback';
 import { getName } from './utils';
 
 describe(getName(__filename), () => {
   beforeEach(() => {
     jest.clearAllMocks();
+    nock.cleanAll;
   });
 
-  it('works', async () => {});
+  afterEach(() => {
+    nock.abortPendingRequests();
+  });
+
+  it('works', async () => {
+    nock('https://web.archive.org/save').post('/example.com').reply(200);
+    const wayback = new WayBack('example.com');
+    await wayback.save();
+    expect(nock.isDone()).toBe(true);
+  });
 });
