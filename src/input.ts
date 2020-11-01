051071f61ed64631ee292842997e1c819f42e56d
Jamie Magee <jamie.magee@gmail.com>
2020-11-01 20:00:24 +0100

    feat: add support for more capture options

--- src/input.ts
+++ src/input.ts
@@ -2,6 +2,9 @@ import * as core from '@actions/core';
 
 export default class Input {
   readonly url = core.getInput('url', { required: true });
+  readonly saveErrors = this.toBoolean(core.getInput('saveErrors'));
+  readonly saveOutlinks = this.toBoolean(core.getInput('saveOutlinks'));
+  readonly saveScreenshot = this.toBoolean(core.getInput('saveScreenshot'));
 
   constructor() {
     this.validate();
@@ -10,6 +13,17 @@ export default class Input {
   validate(): void {
     if (this.url === '') {
       throw new Error('input.url must not be empty');
+    }
+  }
+
+  private toBoolean(input: string): boolean {
+    switch (input.toLowerCase()) {
+      case 'true':
+        return true;
+      case 'false':
+        return false;
+      default:
+        throw new Error();
     }
   }
 }
