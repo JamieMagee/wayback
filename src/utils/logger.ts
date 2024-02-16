373f659fe2e3c09a53197fa7e0dcbed49696d9be
Jamie Magee <jamie.magee@gmail.com>
2024-02-15 21:37:24 -0800

    test: fix logger tests

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,5 @@
 import { error as _error, warning as _warn } from '@actions/core';
 import chalk from 'chalk';
-import stripAnsi from 'strip-ansi';
 
 function write(prefix: string, ...args: unknown[]): void {
   console.log([`[${prefix}]`, ...args].join(' '));
@@ -19,13 +18,13 @@ function info(msg: unknown, ...args: unknown[]): void {
 }
 
 function warn(msg: unknown, ...args: unknown[]): void {
-  write(chalk.magenta('WARN'), msg, ...args);
-  _warn(stripAnsi([msg, ...args].join(' ')));
+  write(chalk.yellow('WARN'), msg, ...args);
+  _warn([msg, ...args].join(' '));
 }
 
 function error(msg: unknown, ...args: unknown[]): void {
   write(chalk.red('ERROR'), msg, ...args);
-  _error(stripAnsi([msg, ...args].join(' ')));
+  _error([msg, ...args].join(' '));
 }
 
 const log = (m: unknown, ...args: unknown[]): void => debug(m, ...args);
--- test/utils/logger.spec.ts
+++ test/utils/logger.spec.ts
@@ -1,5 +1,4 @@
 import * as _core from '@actions/core';
-import chalk from 'chalk';
 import log from '../../src/utils/logger';
 import { getName, mocked } from '../utils';
 
@@ -19,9 +18,9 @@ describe(getName(__filename), () => {
   it('works', () => {
     log('test');
     log.dir({ name: 'test', code: 1 });
-    log.info(chalk.gray('test'), 'it');
-    log.warn(chalk.yellow('test'), 'it');
-    log.error(chalk.red('test'));
+    log.info('test', 'it');
+    log.warn('test', 'it');
+    log.error('test');
 
     expect(logger.mock.calls).toMatchSnapshot();
     expect(core.warning).toHaveBeenCalledWith('test it');
