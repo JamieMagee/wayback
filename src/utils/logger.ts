8f4a1013d8356d5f9e82d7b9e0aa9c63acd7fd5b
Jamie Magee <jamie.magee@gmail.com>
2024-02-15 20:57:24 -0800

    fix: re-add `strip-ansi` dependency to fix build error

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,6 @@
 import { error as _error, warning as _warn } from '@actions/core';
 import chalk from 'chalk';
+import stripAnsi from 'strip-ansi';
 
 function write(prefix: string, ...args: unknown[]): void {
   console.log([`[${prefix}]`, ...args].join(' '));
@@ -19,12 +20,12 @@ function info(msg: unknown, ...args: unknown[]): void {
 
 function warn(msg: unknown, ...args: unknown[]): void {
   write(chalk.magenta('WARN'), msg, ...args);
-  _warn([msg, ...args].join(' '));
+  _warn(stripAnsi([msg, ...args].join(' ')));
 }
 
 function error(msg: unknown, ...args: unknown[]): void {
   write(chalk.red('ERROR'), msg, ...args);
-  _error([msg, ...args].join(' '));
+  _error(stripAnsi([msg, ...args].join(' ')));
 }
 
 const log = (m: unknown, ...args: unknown[]): void => debug(m, ...args);
