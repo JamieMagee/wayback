bf64a2c4efe1b68b7d9ca47132f1a0ea2c566e8d
Nat Welch <nat@natwelch.com>
2026-04-16 16:02:59 +0000

    fix: logging logged stuff as object

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,21 +1,31 @@
+function formatArg(arg: unknown): string {
+  if (typeof arg === 'string') return arg;
+  if (arg instanceof Error) return arg.message;
+  try {
+    return JSON.stringify(arg);
+  } catch {
+    return String(arg);
+  }
+}
+
+function formatMessage(msg: unknown, ...args: unknown[]): string {
+  return [msg, ...args].map(formatArg).join(' ');
+}
+
 function debug(msg: unknown, ...args: unknown[]): void {
-  const message = [msg, ...args].join(' ');
-  console.log(`::debug::${message}`);
+  console.log(`::debug::${formatMessage(msg, ...args)}`);
 }
 
 function info(msg: unknown, ...args: unknown[]): void {
-  const message = [msg, ...args].join(' ');
-  console.log(`::notice::${message}`);
+  console.log(`::notice::${formatMessage(msg, ...args)}`);
 }
 
 function warn(msg: unknown, ...args: unknown[]): void {
-  const message = [msg, ...args].join(' ');
-  console.log(`::warning::${message}`);
+  console.log(`::warning::${formatMessage(msg, ...args)}`);
 }
 
 function error(msg: unknown, ...args: unknown[]): void {
-  const message = [msg, ...args].join(' ');
-  console.log(`::error::${message}`);
+  console.log(`::error::${formatMessage(msg, ...args)}`);
 }
 
 const log = (m: unknown, ...args: unknown[]): void => debug(m, ...args);
